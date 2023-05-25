9b5bc2c32a5dffcd9cdc84cbacd0e30ce50541c2
Tou Chou <tchou@appdev.yearup.org>
2023-05-25 15:28:59 -0400

    added custom user greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello! \n\nWhat is your name?!");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Nice to meet you " + userName + "!");
+
+
     }
 }
